9c51f983c1e569345fa1b44714016172cb3805bf
Wint War Khing <e1045763@u.nus.edu>
2022-12-29 20:02:08 +0800

    my push

--- app/src/main/java/iss/workshop/ca_memorygame/ImageFetchingActivity.java
+++ app/src/main/java/iss/workshop/ca_memorygame/ImageFetchingActivity.java
@@ -3,6 +3,8 @@ package iss.workshop.ca_memorygame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class ImageFetchingActivity extends AppCompatActivity {
 
@@ -11,4 +13,9 @@ public class ImageFetchingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_fetching);
     }
+
+    public void fetchImageClickHandler(View view){
+        Toast.makeText(getApplicationContext(),"Hello Javatpoint",Toast.LENGTH_SHORT).show();
+    }
+
 }
